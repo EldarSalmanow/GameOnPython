abc15654f1239b05562d632b2509c7a0d6894271
eldarsalmanow <eldar.salm@gmail.com>
2020-08-12 16:31:45 +0300

    Отрефакторенный код главного файла. Игра уже работает, но не до конца.

---
+++ src/main.py
@@ -0,0 +1,30 @@
+# импорт генераторов
+from src.generators import genPlayers, genEnemies
+
+# регистрация игроков
+players = genPlayers()
+
+# генерация врагов
+enemies = genEnemies()
+
+# статус игры
+statusGame = True
+
+# итерация цикла игры
+iteration = 0
+
+# главный цикл игры
+while statusGame:
+    iteration += 1
+    # игровой процесс
+    for step in range(0, len(players)):
+        command = str(input(f"Введите команду для игрока {players[step].name}: "))
+        pass
+    # вопрос для игроков о том, закончить ли игру досрочно
+    if iteration % 7 == 0:
+        commandGame = int(input("Если хотите закончить игру, напишите '0', если продолжить, напишите '1': "))
+        if commandGame == 0:
+            statusGame = False
+            print("Игра окончена!")
+        else:
+            continue
